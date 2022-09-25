8dd8fc3a1e153ddc3bcdff1e56373b650cf466ff
Dieter Schmidt <dima.loschkin@gmail.com>
2022-09-25 15:03:17 +0200

    [github actions] reduce build fequency & use consecutive builds instead of parallel

--- build_firmware.sh
+++ build_firmware.sh
@@ -2,15 +2,16 @@
 
 BASE=$(pwd)
 echo $BASE
-TARGET=linux
+TARGET=cheetah
+BUILD_DIR=${BASE}/build
 CONFIG_NAME=".config_${TARGET}"
-CONFIG_PATH=${BASE}/${CONFIG_NAME}
+CONFIG_PATH=${BUILD_DIR}/${CONFIG_NAME}
 touch ${CONFIG_PATH}
-BUILD_DIR=${BASE}/klipper_build_${TARGET}
+OUT_DIR=${BUILD_DIR}/klipper_build_${TARGET}
 mkdir -p ${BUILD_DIR}
 CONTAINER=$(docker run -d -v ${CONFIG_PATH}:/home/klippy/klipper/.config -v ${BUILD_DIR}:/home/klippy/klipper/out klipper-builder sleep infinity)
 docker exec -it ${CONTAINER} make menuconfig
 docker exec ${CONTAINER} make
-docker exec ${CONTAINER} make flash
+#docker exec ${CONTAINER} make flash
 docker stop ${CONTAINER}
 docker rm ${CONTAINER}
